8a6b4262b5d5c797cd747a6bda87068077fb41e1
Thomas St Pierre <tstpierre@nothingsoftstudios.com>
2014-10-24 16:55:57 -0700

    Create Xamarin Forms PCL project with updated packages

---
+++ NeuChat/NeuChat/NeuChat.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using NeuChat.WinPhone.Resources;
+
+namespace NeuChat.WinPhone {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
