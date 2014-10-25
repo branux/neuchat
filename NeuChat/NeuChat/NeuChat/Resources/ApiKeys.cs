d00ad9726917726f3fb5046dfac3d59ad1a3ba14
Thomas St Pierre <tstpierre@nothingsoftstudios.com>
2014-10-25 01:05:16 -0700

    Removing api keys from commits

--- NeuChat/NeuChat/NeuChat/Resources/ApiKeys.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NeuChat.Resources {
-    public sealed class ApiKeys {
-        public const string AZURE_URL = "https://neuchat.azure-mobile.net/";
-        public const string AZURE_KEY = "qZdhBTBfJOVhyKnOxkLPVGHfTarmEq64";
-    }
-}
